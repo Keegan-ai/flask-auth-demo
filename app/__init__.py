cec3cbf62f5db929df055bb6b77de8d2e7b9a842
Keegan-ai <kgous@waimea.school.nz>
2025-06-10 10:01:18 +1200

    Made my login work

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 # App Creation and Launch
 #===========================================================
 
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, session, request, flash, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 import html
 
@@ -43,6 +43,13 @@ def about():
 @app.get("/signup/")
 def signup():
     return render_template("pages/signup.jinja")
+
+#-----------------------------------------------------------
+# Login Page Route
+#-----------------------------------------------------------
+@app.get("/login/")
+def login():
+    return render_template("pages/login.jinja")
 
 #-----------------------------------------------------------
 # Things page route - Show all the things, and new thing form
@@ -142,6 +149,48 @@ def add_a_user():
         # Go back to the home page
         flash(f"User  '{name}' added", "success")
         return redirect("/")
+    
+
+#-----------------------------------------------------------
+# Route for logging in a user, using data posted from a form
+#-----------------------------------------------------------
+@app.post("/login-user")
+def login_user():
+    # Get the data from the form
+    username  = request.form.get("username")
+    password  = request.form.get("password")
+  
+
+    # Sanitise the inputs
+    username = html.escape(username)
+
+
+
+    with connect_db() as client:
+        # Try to find a matching record 
+        sql = "SELECT id, name, password_hash FROM users WHERE username= ?"
+        values = [username]
+        result = client.execute(sql, values)
+
+        #Check if we got a record
+        if result.rows:
+            #Yes, User exist
+            user = result.row[0]
+            hash = user["password_hash"]
+
+            #Check if passwords match
+            if check_password_hash(hash, password):
+                #Yes,so save the detail in the session.
+                session["user_id"] = user["id"]
+                session["user_name"] = user["name"]
+                flash("Logged in succesfully", "success")
+                return redirect("/")
+
+
+        # Go back to the home page
+        flash("Incorrect credentials", "error")
+        return redirect("/login")
+
 
 
 #-----------------------------------------------------------
